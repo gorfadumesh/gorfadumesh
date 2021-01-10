5c0ad7b2cbfbb76c480be5d4db5938436eceb844
gorfadumesh <gorfadumesh1213@gmail.com>
2021-01-10 11:19:53 +0530

    changes in title and add watsapp

--- umeshgorfad/gatsby-config.js
+++ umeshgorfad/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Full Stack Developer`,
+    title: `Web Developer`,
     description: `My personal portfolio website`,
     author: `@umeshg`,
   },
