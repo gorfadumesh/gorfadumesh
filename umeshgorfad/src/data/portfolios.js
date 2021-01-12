78170a285e0ff01861850651be330d31c60bbfed
gorfadumesh <gorfadumesh1213@gmail.com>
2021-01-12 18:50:49 +0530

    portfolio add

--- umeshgorfad/src/data/portfolios.js
+++ umeshgorfad/src/data/portfolios.js
@@ -1,14 +1,25 @@
 export const portfolios = {
   Web: [
-    // {
-    //   type: "featured",
-    //   name: "Gradienta",
-    //   image: require("./image/gradienta.jpg"),
-    //   description:
-    //     "A Collection of free Multicolor CSS, JPG, PNG, SVG Gradients with a Gradienta Editor",
-    //   demo: "https://gradienta.io",
-    //   tools: ["gatsby", "netlify", "ga", "forestry"],
-    // },
+    {
+      type: "regular",
+      name: "Expense Tracker",
+      image: require("./image/ExpenseTracker.jpg"),
+      description:
+        "The simple income and expense calulation app with react, local storage and react hooks",
+      demo: "https://expensetrackerumesh.netlify.app/",
+      source: "https://github.com/gorfadumesh/Expense_Tracker",
+      tools: ["reactjs", "netlify"],
+    },
+    {
+      type: "regular",
+      name: "Expense Tracker",
+      image: require("./image/ExpenseTracker.jpg"),
+      description:
+        "simple community-building room chat app built using react, firebase, and react-firebase-hooks",
+      demo: "https://umesh-chat-app.web.app/",
+      source: "https://github.com/gorfadumesh/pkmconnect",
+      tools: ["reactjs", "firebase"],
+    },
     // {
     //   type: "featured",
     //   name: "DevSonket",
@@ -16,119 +27,119 @@ export const portfolios = {
     //   description:
     //     "A Collection of Bengali Cheatsheets for the developers, completely open-source",
     //   demo: "https://devsonket.com",
-    //   source: "https://github.com/devsonket/devsonket.github.io",
+    //   source: "https://github.com/gorfadumesh/Expense_Tracker",
     //   tools: ["gatsby", "json", "travis ci"],
     // },
-    // {
-    //   type: "featured",
-    //   name: "js.umeshgorfad.netlify.app",
-    //   image: require("./image/js.umeshgorfad.netlify.app.png"),
-    //   description:
-    //     "Learn Practical use case of JavaScript in your everyday life, entirely in Bengali",
-    //   demo: "https://umesh.js.org",
-    //   source: "https://github.com/zonayedpca/js.umeshgorfad.netlify.app",
-    //   tools: ["gatsby", "utterances", "travis ci"],
-    // },
-    // {
-    //   type: "regular",
-    //   name: "Phonebook",
-    //   description:
-    //     "A CRUD Application to store your contact phone numbers, everything at one place",
-    //   demo: "https://phonebookpca.netlify.com/",
-    //   source: "https://github.com/zonayedpca/nodeMySQLPhonebook",
-    //   tools: ["express", "mysql", "react"],
-    // },
-    // {
-    //   type: "regular",
-    //   name: "Holy Quran",
-    //   description:
-    //     "Read the Holy Quran with English Transaltion. Simple, basic and clean",
-    //   demo: "https://zonayedpca.github.io/HolyQuranReact/",
-    //   source: "https://github.com/zonayedpca/HolyQuranReact",
-    //   tools: ["react", "redux"],
-    // },
-    // {
-    //   type: "regular",
-    //   name: "To-do App",
-    //   description:
-    //     "Just another interactive, nice, beautiful and small to-do application. Does everything!",
-    //   demo: "https://todoreacthooks.netlify.com/",
-    //   source: "https://github.com/zonayedpca/todo-react-hooks",
-    //   tools: ["react", "react hooks"],
-    // },
-    // {
-    //   type: "regular",
-    //   name: "Micro Blog",
-    //   description:
-    //     "Entirely works on the client-side, behaves like actual micro blog, everything handled by client",
-    //   demo: "https://zonayedpca.github.io/react-micro-blog/",
-    //   source: "https://github.com/zonayedpca/react-micro-blog",
-    //   tools: ["react", "react router"],
-    // },
-    {
-      type: "regular",
-      name: "Mini Blog",
-      description:
-        "Entirely works on the client-side, behaves like actual blog, can even make posts, bookmarks",
-      demo: "https://zonayedpca.github.io/react-mini-blog/",
-      source: "https://github.com/zonayedpca/react-mini-blog",
-      tools: ["react", "react router", "draft js"],
-    },
-  ],
-  Others: [
-    {
-      type: "featured",
-      name: "DevTop",
-      image: require("./image/devtop.jpg"),
-      description:
-        "Cross-platform desktop tool for the developers, does some useful job and make life easy",
-      demo: "https://zonayedpca.github.io/DevTop",
-      source: "https://github.com/zonayedpca/DevTop",
-      tools: ["electron", "react"],
-    },
-    // {
-    //   type: "featured",
-    //   name: "Biswasi",
-    //   image: require("./image/biswasi.jpg"),
-    //   description:
-    //     "Cross-platform Mobile app for the Muslim people to remind them about their prayer",
-    //   demo: "https://play.google.com/store/apps/details?id=com.biswasi",
-    //   tools: ["react native", "redux"],
-    // },
-    // {
-    //   type: "featured",
-    //   name: "AlgoDS.js",
-    //   image: require("./image/algods.jpg"),
-    //   description:
-    //     "A Collection of most used algorithms and data structures implemented using JavaScript",
-    //   source: "https://github.com/zonayedpca/AlgoDS.js",
-    //   tools: ["data structure", "algorithm"],
-    // },
-    // {
-    //   type: "regular",
-    //   name: "React Native Welcome",
-    //   description:
-    //     "A helper package for React Native to welcome user with beautiful and interactive UI",
-    //   demo: "https://www.npmjs.com/package/react-native-welcome",
-    //   source: "https://github.com/zonayedpca/react-native-welcome",
-    //   tools: ["npm package", "react native"],
-    // },
-    // {
-    //   type: "regular",
-    //   name: "React Web Workers",
-    //   description: "A Small helper package for React to work with web workers",
-    //   demo: "https://www.npmjs.com/package/react-web-workers",
-    //   source: "https://github.com/zonayedpca/react-web-workers",
-    //   tools: ["npm package", "react"],
-    // },
-    // {
-    //   type: "regular",
-    //   name: "React Poll",
-    //   description:
-    //     "Entirely works on client-side, behave like actual poll application",
-    //   demo: "https://zonayedpca.github.io/react-fake-poll/",
-    //   source: "https://github.com/zonayedpca/react-fake-poll",
-    //   tools: ["react", "react router"],
-    // },
+    //   {
+    //     type: "featured",
+    //     name: "js.umeshgorfad.netlify.app",
+    //     image: require("./image/js.umeshgorfad.netlify.app.png"),
+    //     description:
+    //       "Learn Practical use case of JavaScript in your everyday life, entirely in Bengali",
+    //     demo: "https://umesh.js.org",
+    //     source: "https://github.com/zonayedpca/js.umeshgorfad.netlify.app",
+    //     tools: ["gatsby", "utterances", "travis ci"],
+    //   },
+    //   {
+    //     type: "regular",
+    //     name: "Phonebook",
+    //     description:
+    //       "A CRUD Application to store your contact phone numbers, everything at one place",
+    //     demo: "https://phonebookpca.netlify.com/",
+    //     source: "https://github.com/zonayedpca/nodeMySQLPhonebook",
+    //     tools: ["express", "mysql", "react"],
+    //   },
+    //   {
+    //     type: "regular",
+    //     name: "Holy Quran",
+    //     description:
+    //       "Read the Holy Quran with English Transaltion. Simple, basic and clean",
+    //     demo: "https://zonayedpca.github.io/HolyQuranReact/",
+    //     source: "https://github.com/zonayedpca/HolyQuranReact",
+    //     tools: ["react", "redux"],
+    //   },
+    //   {
+    //     type: "regular",
+    //     name: "To-do App",
+    //     description:
+    //       "Just another interactive, nice, beautiful and small to-do application. Does everything!",
+    //     demo: "https://todoreacthooks.netlify.com/",
+    //     source: "https://github.com/zonayedpca/todo-react-hooks",
+    //     tools: ["react", "react hooks"],
+    //   },
+    //   {
+    //     type: "regular",
+    //     name: "Micro Blog",
+    //     description:
+    //       "Entirely works on the client-side, behaves like actual micro blog, everything handled by client",
+    //     demo: "https://zonayedpca.github.io/react-micro-blog/",
+    //     source: "https://github.com/zonayedpca/react-micro-blog",
+    //     tools: ["react", "react router"],
+    //   },
+    //   {
+    //     type: "regular",
+    //     name: "Mini Blog",
+    //     description:
+    //       "Entirely works on the client-side, behaves like actual blog, can even make posts, bookmarks",
+    //     demo: "https://zonayedpca.github.io/react-mini-blog/",
+    //     source: "https://github.com/zonayedpca/react-mini-blog",
+    //     tools: ["react", "react router", "draft js"],
+    //   },
+    // ],
+    // Others: [
+    //   {
+    //     type: "featured",
+    //     name: "DevTop",
+    //     image: require("./image/devtop.jpg"),
+    //     description:
+    //       "Cross-platform desktop tool for the developers, does some useful job and make life easy",
+    //     demo: "https://zonayedpca.github.io/DevTop",
+    //     source: "https://github.com/zonayedpca/DevTop",
+    //     tools: ["electron", "react"],
+    //   },
+    //   {
+    //     type: "featured",
+    //     name: "Biswasi",
+    //     image: require("./image/biswasi.jpg"),
+    //     description:
+    //       "Cross-platform Mobile app for the Muslim people to remind them about their prayer",
+    //     demo: "https://play.google.com/store/apps/details?id=com.biswasi",
+    //     tools: ["react native", "redux"],
+    //   },
+    //   {
+    //     type: "featured",
+    //     name: "AlgoDS.js",
+    //     image: require("./image/algods.jpg"),
+    //     description:
+    //       "A Collection of most used algorithms and data structures implemented using JavaScript",
+    //     source: "https://github.com/zonayedpca/AlgoDS.js",
+    //     tools: ["data structure", "algorithm"],
+    //   },
+    //   {
+    //     type: "regular",
+    //     name: "React Native Welcome",
+    //     description:
+    //       "A helper package for React Native to welcome user with beautiful and interactive UI",
+    //     demo: "https://www.npmjs.com/package/react-native-welcome",
+    //     source: "https://github.com/zonayedpca/react-native-welcome",
+    //     tools: ["npm package", "react native"],
+    //   },
+    //   {
+    //     type: "regular",
+    //     name: "React Web Workers",
+    //     description: "A Small helper package for React to work with web workers",
+    //     demo: "https://www.npmjs.com/package/react-web-workers",
+    //     source: "https://github.com/zonayedpca/react-web-workers",
+    //     tools: ["npm package", "react"],
+    //   },
+    //   {
+    //     type: "regular",
+    //     name: "React Poll",
+    //     description:
+    //       "Entirely works on client-side, behave like actual poll application",
+    //     demo: "https://zonayedpca.github.io/react-fake-poll/",
+    //     source: "https://github.com/zonayedpca/react-fake-poll",
+    //     tools: ["react", "react router"],
+    //   },
   ],
 }
--- umeshgorfad/src/pages/index.js
+++ umeshgorfad/src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = () => (
       <About />
       <Skill />
       <LoveToDo />
-      {/* <Portfolio /> */}
+      <Portfolio />
       <Contact />
     </Layout>
   </>
